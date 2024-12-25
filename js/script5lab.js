f8443c39ebc5774bd675b32ae70cff032a6335d4
Linokan <Andrei125.ivanov@mail.ru>
2024-12-25 20:43:57 +0700

    7lab

--- js/script5lab.js
+++ js/script5lab.js
@@ -86,22 +86,6 @@ function choosePath(path) {
         case 3:
         case 4:
         case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-        case 21:
             nextStage(); // переход к следующему этапу
             break;
     }
